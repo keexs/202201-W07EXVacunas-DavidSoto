7e362779b132a8b1e765709c5534ca965b8e7ad9
David Soto <davidsotosendarrubias@gmail.com>
2022-02-24 17:05:11 +0100

    final push

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,8 @@ const Ciudad = require("../db/models/Ciudad");
 
 const getCentros = async () => {
   const ciudad = await Ciudad.find();
-  console.log(ciudad);
+  const centros = ciudad[0].puntosVacunacion;
+  centros.map((centro) => console.log(centro.nombre));
 };
 
 module.exports = getCentros;
